238dfe323e3dffb3fec70e29a3f454bc6781957a
Gabriel Brun <lydstyl@gmail.com>
2022-10-17 01:38:41 +0200

    Delete a book.

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import * as dotenv from "dotenv"
 dotenv.config()
 import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from "@apollo/server/standalone"
-import books from "./data/books.js"
 import { connectDB } from "./config/db.js"
-import Book from './models/Book.js'
+import Book from "./models/Book.js"
 
 connectDB()
 
 const typeDefs = `
     type Book {
+        id:ID
         title: String
         author: String
     }
@@ -21,52 +21,30 @@ const typeDefs = `
 
     type Mutation {
         addBook(title: String, author: String): Book
+        deleteBook(id: String): Book
     }
 `
 
 const resolvers = {
-    // Query: {
-    //     books: async () => {
-            
-    //         // console.log(Book.find());
-
-    //         const result = await Book.find()
-            
-            
-    //         return books},
-    // },
     Query: {
-        books:  async () => {
+        books: async () => {
             const result = await Book.find()
-            console.log(result);
-            // return books
             return result
         },
     },
     Mutation: {
-        addBook: (_, { title, author }) => { // parent and args ?
-            // const newBook = {
-            //     title,
-            //     author,
-            // }
-            // // // in graphi :
-            // // mutation AddBook {
-            // //     addBook(title: "Gabise book", author: "Gab") {
-            // //       title
-            // //       author
-            // //     }
-            // //   }
-
-            // books.push(newBook)
-
-            const newBook = new Book({
-                title: 'Mon titre', author: 'Gaby'
+        addBook: (_, { title, author }) => {
+            // parent and args ?
+            const newItem = new Book({
+                title,
+                author,
             })
-            newBook.save()
-
-            console.log(`Book ${newBook.title} added.`)
-
-            return newBook
+            newItem.save()
+            return newItem
+        },
+        deleteBook: async (_, { id }) => {
+            const result = await Book.findByIdAndRemove(id)
+            return result
         },
     },
 }
@@ -75,9 +53,7 @@ const server = new ApolloServer({
     typeDefs,
     resolvers,
 })
-
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
 })
-
-console.log(colors.green.underline.bold`🚀  Server ready at: ${url}`))
+console.log(colors.green.underline.bold`🚀  Server ready at: ${url}`)
