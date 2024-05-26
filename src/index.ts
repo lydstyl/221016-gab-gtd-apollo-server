a52e344107724ab514a5bf906327c3398a892ee7
Gabriel Brun <lydstyl@gmail.com>
2024-05-26 11:42:02 +0200

    Change port to 5000 by default for render in production.

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,50 @@
-import * as dotenv from "dotenv"
+import * as dotenv from 'dotenv'
 dotenv.config()
-import colors from "colors"
-import jwt from "jsonwebtoken"
-import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
-import { connectDB } from "./config/db.js"
-import typeDefs from "./typeDefs/all.js"
-import resolvers from "./resolvers/all.js"
-import { MyContext } from "./types.js"
+import colors from 'colors'
+import jwt from 'jsonwebtoken'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { connectDB } from './config/db.js'
+import typeDefs from './typeDefs/all.js'
+import resolvers from './resolvers/all.js'
+import { MyContext } from './types.js'
 
 const server = new ApolloServer<MyContext>({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers
 })
 
 connectDB()
 
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: async ({ req, res }) => {
-        // verify a token symmetric - synchronous
-        try {
-            const decoded = jwt.verify(
-                req.headers.authorization,
-                process.env.JWT_SECRET
-            )
-            const { email } = decoded
-            const theContext: MyContext = {
-                authScope: "user",
-                email,
-            }
-            if (email === process.env.ADMIN_EMAIL) {
-                theContext.authScope = "admin"
-                return theContext
-            } else {
-                return theContext
-            }
-        } catch (err) {
-            const unauthorisedContext: MyContext = {
-                authScope: "unauthorised",
-                email: "invalid token",
-            }
-            return unauthorisedContext
-        }
-        return {}
-    },
+  listen: { port: +process.env.PORT || 5000 },
+  context: async ({ req, res }) => {
+    // verify a token symmetric - synchronous
+    try {
+      const decoded = jwt.verify(
+        req.headers.authorization,
+        process.env.JWT_SECRET
+      )
+      const { email } = decoded
+      const theContext: MyContext = {
+        authScope: 'user',
+        email
+      }
+      if (email === process.env.ADMIN_EMAIL) {
+        theContext.authScope = 'admin'
+        return theContext
+      } else {
+        return theContext
+      }
+    } catch (err) {
+      const unauthorisedContext: MyContext = {
+        authScope: 'unauthorised',
+        email: 'invalid token'
+      }
+      return unauthorisedContext
+    }
+    return {}
+  }
 })
 // console.log(colors.green.underline.bold`🚀  Server ready at: ${url}`)
 console.log(`🚀  Server ready at: ${url}`)
